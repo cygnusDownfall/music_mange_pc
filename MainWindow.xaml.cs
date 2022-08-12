a9777c92aa82235b9b4cc5e9201bbf474fbae3ca
CygnusDownfall <phapsuhacam112@gmail.com>
2022-08-12 19:59:06 +0700

    12/08/2022 mediaplayer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace music_manage
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        public void changetotablistmusic(object sender,RoutedEventArgs e)
+        {
+            listmusic.Visibility =Visibility.Visible;
+        }
+        MediaPlayer player;
+        public void musiclvclick(object sender, RoutedEventArgs e)
+        {
+            string stringuri = "https://zingmp3.vn/embed/playlist/6BUOOID6";
+
+            //khoi chay media thanh cong 
+            player = new MediaPlayer();
+            Uri source = new Uri(stringuri);
+            player.Open(source);
+            //MessageBox.Show("khoi tao thanh cong");//da thanh cong 
+
+            //bug:::play khong thanh cong
+            player.Play();
+        }
+        public void exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ Model/timestation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manage.Model
+{
+    public class timestation
+    {
+
+        DateTime time;
+        string Mota { get; set; }
+        timestation()
+        {
+
+        }
+    }
+    public class timestationcontroller { 
+        public void Travel(timestation timestation)
+        {
+
+        }
+    }
+    public class music
+    {
+        string path { get; set; }
+
+    }
+}
